e7019f56061d2fe25296529664d31e3fab8fb7ab
Blaire Swaby-Clacken <blaireswabyclacken@yahoo.com>
2022-07-14 11:24:16 -0400

    toggle show password function changed to allow function use in both login and registration form

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-let passwordInput = document.getElementById('password');
-let eyeIcon = document.getElementById('eyeIcon');
+const passwordInputs = document.querySelectorAll('#password');
+const toggleShow = document.querySelectorAll('#toggleShow');
 const container = document.querySelector('.container'); 
 const logIn = document.querySelector('.login-link');
 const signUp = document.querySelector('.signup-link');
 
 
 //Function that toggles showing the password entered
-eyeIcon.onclick = function() {
-    const inputType = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordInput.type = inputType;
-    
-    if (this.classList.contains('fa-eye-slash')) {
-        this.classList.remove('fa-eye-slash');
-        this.classList.add('fa-eye');
-    } else {
-        this.classList.remove('fa-eye');
-        this.classList.add('fa-eye-slash');
-    }
-};
+toggleShow.forEach(eyeIcon =>{
+    eyeIcon.addEventListener('click', ()=>{
+        passwordInputs.forEach(pwInput =>{
+            if(pwInput.type === 'password'){
+                pwInput.type = 'text';
+
+                toggleShow.forEach(icon =>{
+                    icon.classList.replace('fa-eye-slash', 'fa-eye');
+                })
+            }else{
+                pwInput.type = 'password';
+
+                toggleShow.forEach(icon =>{
+                    icon.classList.replace('fa-eye','fa-eye-slash');
+                })
+            }
+        })
+    })
+})
+
 
 
 //Appear Signup & Login
